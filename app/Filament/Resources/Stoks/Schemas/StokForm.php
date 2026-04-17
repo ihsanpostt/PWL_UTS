bd72f75f62d666ae1d6e6b848bc2cdb7b365acd1
Burhanuddin ihsan <echan5886@gmail.com>
2026-04-17 18:13:28 +0700

    set up CRUD Stok

---
+++ app/Filament/Resources/Stoks/Schemas/StokForm.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace App\Filament\Resources\Stoks\Schemas;
+
+use Filament\Schemas\Schema;
+
+class StokForm
+{
+    public static function configure(Schema $schema): Schema
+    {
+        return $schema
+            ->components([
+                //
+            ]);
+    }
+}
---
+++ app/Filament/Resources/Stoks/Tables/StoksTable.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace App\Filament\Resources\Stoks\Tables;
+
+use Filament\Actions\BulkActionGroup;
+use Filament\Actions\DeleteBulkAction;
+use Filament\Actions\EditAction;
+use Filament\Tables\Table;
+
+class StoksTable
+{
+    public static function configure(Table $table): Table
+    {
+        return $table
+            ->columns([
+                //
+            ])
+            ->filters([
+                //
+            ])
+            ->recordActions([
+                EditAction::make(),
+            ])
+            ->toolbarActions([
+                BulkActionGroup::make([
+                    DeleteBulkAction::make(),
+                ]),
+            ]);
+    }
+}
